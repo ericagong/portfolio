a49d95bf8a3bec130dd651adc69bc6554290dc89
ericagong <48196721+ericagong@users.noreply.github.com>
2023-05-19 18:36:57 +0900

    feat: 배너 skeleton 구현

--- js/main.js
+++ js/main.js
@@ -19,3 +19,28 @@ serachInputElement.addEventListener("blur", function () {
   serachInputElement.setAttribute("placeholder", "");
   serachInputElement.value = "";
 });
+
+const badgesElement = document.querySelector("header .badges");
+
+// lodash library 사용해 throttle 기능 구현
+// gsap library 사용해 애니메이션 구현
+// 실제로 요소 삭제도 필요
+window.addEventListener(
+  "scroll",
+  _.throttle(function () {
+    if (window.scrollY > 500) {
+      // badges 숨기기
+      // gsap.to(요소, 지속시간, 옵션);
+      gsap.to(badgesElement, 0.6, {
+        opacity: 0,
+        display: "none",
+      });
+    } else {
+      // badges 보이기
+      gsap.to(badgesElement, 0.6, {
+        opacity: 1,
+        display: "block",
+      });
+    }
+  }, 300)
+);
